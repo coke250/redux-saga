5ed0eec6b959c77dcb0f4ad643caf2e267dbadd9
SSABI <fhy1930@gmail.com>
2021-10-14 00:13:41 +0900

    리듀서, 사가 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import createSagaMiddleware from 'redux-saga'
+import { applyMiddleware, createStore } from 'redux'
+import rootReducer, { rootSaga } from './modules'
+import { Provider } from 'react-redux'
+
+// saga middleware 생성
+const sagaMiddleware = createSagaMiddleware()
+
+// 스토어 생성
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+
+// saga middleware 실행
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>,
+  document.getElementById('root'),
+)
